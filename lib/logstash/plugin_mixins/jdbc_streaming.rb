4640b44c763674e923d9d6d892b0cea3e5544bdb
Jake Landis <jake.landis@elastic.co>
2018-03-26 12:05:54 -0500

    Load the driver with the system class loader. Fixes issue loading some JDBC drivers in Logstash 6.2+
    Related to: [JDBC input - #263](https://github.com/logstash-plugins/logstash-input-jdbc/issues/263)
    
    Fixes #10

--- lib/logstash/plugin_mixins/jdbc_streaming.rb
+++ lib/logstash/plugin_mixins/jdbc_streaming.rb
@@ -43,7 +43,12 @@ module LogStash module PluginMixins module JdbcStreaming
     require "sequel"
     require "sequel/adapters/jdbc"
     require "java"
-    require @jdbc_driver_library if @jdbc_driver_library
+
+    if @jdbc_driver_library
+      class_loader = java.lang.ClassLoader.getSystemClassLoader().to_java(java.net.URLClassLoader)
+      class_loader.add_url(java.io.File.new(@jdbc_driver_library).toURI().toURL())
+    end
+
     Sequel::JDBC.load_driver(@jdbc_driver_class)
     @database = Sequel.connect(@jdbc_connection_string, :user=> @jdbc_user, :password=>  @jdbc_password.nil? ? nil : @jdbc_password.value)
     if @jdbc_validate_connection
