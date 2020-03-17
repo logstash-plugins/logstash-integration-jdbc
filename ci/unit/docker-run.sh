19161ff9137935a7f4b142c246dc35a3f65767de
Joao Duarte <jsvduarte@gmail.com>
2020-03-17 22:42:52 +0000

    move testing to centralized travis configuration

--- ci/unit/docker-run.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# This is intended to be run inside the docker container as the command of the docker-compose.
-set -ex
-docker-compose -f ci/unit/docker-compose.yml up --exit-code-from logstash
