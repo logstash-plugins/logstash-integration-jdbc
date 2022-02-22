faf80d8a4103b5ac37d6029f050a9ecc3c4faac0
kares <self@kares.org>
2022-02-22 14:43:10 +0100

    Refactor: scheduler api in preparation for re-use

--- lib/logstash/plugin_mixins/jdbc/scheduler.rb
+++ lib/logstash/plugin_mixins/jdbc/scheduler.rb
@@ -12,6 +12,34 @@ module LogStash module PluginMixins module Jdbc
     TimeImpl = defined?(Rufus::Scheduler::EoTime) ? Rufus::Scheduler::EoTime :
                    (defined?(Rufus::Scheduler::ZoTime) ? Rufus::Scheduler::ZoTime : ::Time)
 
+    # @param cron [String] cron-line
+    # @param opts [Hash] scheduler options
+    # @return scheduler instance
+    def self.start_cron_scheduler(cron, opts = {}, &block)
+      unless block_given?
+        raise ArgumentError, 'missing (cron scheduler) block - worker task to execute'
+      end
+      scheduler = new_scheduler(opts)
+      scheduler.schedule_cron(cron, &block)
+      scheduler
+    end
+
+    # @param opts [Hash] scheduler options
+    # @return scheduler instance
+    def self.new_scheduler(opts)
+      unless opts.key?(:thread_name)
+        raise ArgumentError, 'thread_name: option is required to be able to distinguish multiple scheduler threads'
+      end
+      opts[:max_work_threads] ||= 1
+      # amount the scheduler thread sleeps between checking whether jobs
+      # should trigger, default is 0.3 which is a bit too often ...
+      # in theory the cron expression '* * * * * *' supports running jobs
+      # every second but this is very rare, we could potentially go higher
+      opts[:frequency] ||= 1.0
+
+      new(opts)
+    end
+
     # @overload
     def timeout_jobs
       # Rufus relies on `Thread.list` which is a blocking operation and with many schedulers
