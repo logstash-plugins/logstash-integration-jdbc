6b26703d09512acc664480154d9614df67258fac
andsel <selva.andre@gmail.com>
2026-08-24 16:28:39 +0200

    Reapply "Reapply "Minor, removed commented code""
    This reverts commit 2029f5bb2ac7c1a4b91f13ca21798148e65b9cb2.

--- lib/logstash/plugin_mixins/jdbc/statement_handler.rb
+++ lib/logstash/plugin_mixins/jdbc/statement_handler.rb
@@ -140,10 +140,6 @@ module LogStash module PluginMixins module Jdbc
     # @yieldparam row [Hash{Symbol=>Object}]
     def perform_query(db, sql_last_value)
       build_query(db, sql_last_value) { |row| yield row }
-      # query = build_query(db, sql_last_value) { |row| yield row }
-      # query.each do |row|
-      #   yield row
-      # end
     end
 
     private
